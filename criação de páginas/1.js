0571faeb76f5be7acdc75537cd486540de5fc0de
niks-b1 <nikolasbreno.vs@gmail.com>
2025-12-02 15:51:24 -0300

    criacao de paginas

---
+++ criação de páginas/1.js
@@ -0,0 +1,6 @@
+let botao = document.getElementById("botao")
+let biografia = document.getElementById("sobre-mim")
+
+botao.addEventListener("click", () => {
+    biografia.style.display = "inline"
+})
--- relógio/script.js
+++ relógio/script.js
@@ -71,9 +71,15 @@ let temporizador = document.getElementById("temporizador")
 let tempo = document.getElementById("tempo")
 let botao = document.getElementById("botao")
 
-let tempo_temporizador
+let tempo_temporizador = [0, 0, 0]
 function atualizartemporizador(){
     tempo_temporizador[2]--
+
+    if(tempo_temporizador[0] == 0 && tempo_temporizador[1] == 0 && tempo_temporizador[2] == 0){
+        clearInterval(id_temporizador)
+        alert("acabou o tempo")
+        ativo_temporizador = false
+    }
     
     if(tempo_temporizador[2] == -1){
         tempo_temporizador[2] = 59 
@@ -88,10 +94,19 @@ function atualizartemporizador(){
 }
 
 let ativo_temporizador = false
+let id_temporizador
 botao.addEventListener("click", () => {
     if(ativo_temporizador == false){
-        tempo_temporizador = [0, tempo.value, 0]
-        setInterval(atualizartemporizador, 1000)
+        if(tempo_temporizador[0] == 0 && tempo_temporizador[1] == 0 && tempo_temporizador[2] == 0){
+            tempo_temporizador = [0, tempo.value, 0]
+        }
+        id_temporizador = setInterval(atualizartemporizador, 1000)
         ativo_temporizador = true
+        botao.innerText = "pausar"
+    }
+    else{
+        clearInterval(id_temporizador)
+        ativo_temporizador = false
+        botao.innerText = "iniciar"
     }
 })
