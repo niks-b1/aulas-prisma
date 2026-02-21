229a2fd0f74d2e0969e672cf0c1eae1bcfe366af
niks-b1 <nikolasbreno.vs@gmail.com>
2026-02-21 12:12:01 -0300

    campeonato futebol POO

--- POO/campeonato_futebol.js
+++ POO/campeonato_futebol.js
@@ -54,35 +54,54 @@ let campeonato = new Campeonato()
 let input = document.getElementById("nomedotime")
 let botaotime = document.getElementById("adicionar")
 
-function timenatabela(){
-    let tabela = document.getElementById("tabela")
-    let novalinha = document.createElement("tr")
-    let timeatual = campeonato.times[campeonato.times.length -1]
-    novalinha.innerHTML = 
-        "<td>" + timeatual.nome + "</td>" +
-        "<td>" + timeatual.calcularpontos() + "</td>" +
-        "<td>" + timeatual.vitoria + "</td>" +
-        "<td>" + timeatual.empate + "</td>" +
-        "<td>" + timeatual.derrota + "</td>" +
-        "<td>" + timeatual.golsmarcados + "</td>" +
-        "<td>" + timeatual.golssofridos + "</td>" +
-        "<td>" + timeatual.calcularsaldo() +"</td>"
-    console.log(novalinha);
-    tabela.appendChild(novalinha)
-    
+let tabela = document.getElementById("tabela")
+let timeatual = campeonato.times[campeonato.times.length -1]
+function atualizartabela(){
+    tabela.innerHTML = "<tr><th>Time</th><th>Pontos</th><th>Vitória</th><th>Empates</th><th>Derrotas</th><th>Gols marcados</th><th>Gols sofridos</th><th>Saldo de gols</th></tr>"
+    for(let contador = 0;contador < campeonato.times.length;contador ++){
+        let novalinha = document.createElement("tr")
+        novalinha.innerHTML = 
+            "<td>" + campeonato.times[contador].nome + "</td>" +
+            "<td>" + campeonato.times[contador].calcularpontos() + "</td>" +
+            "<td>" + campeonato.times[contador].vitoria + "</td>" +
+            "<td>" + campeonato.times[contador].empate + "</td>" +
+            "<td>" + campeonato.times[contador].derrota + "</td>" +
+            "<td>" + campeonato.times[contador].golsmarcados + "</td>" +
+            "<td>" + campeonato.times[contador].golssofridos + "</td>" +
+            "<td>" + campeonato.times[contador].calcularsaldo() +"</td>"
+        tabela.appendChild(novalinha)
+    }
 }
 function atualizaOpcoes(){
-    let selecionarcasa = document.getElementById("casa")
-    let selecionarvisitante = document.getElementById("visitante")
-
+    let selecionartime = document.querySelectorAll(".selecionar")
+    let novaopcao = document.createElement("option")
+    let novaopcao2 = document.createElement("option")
+    let opcaoatual = campeonato.times[campeonato.times.length -1]
+    novaopcao.innerHTML = opcaoatual.nome
+    novaopcao2.innerHTML = opcaoatual.nome
+    selecionartime[0].appendChild(novaopcao)
+    selecionartime[1].appendChild(novaopcao2)
 }
 
 botaotime.addEventListener("click", () => {
     campeonato.adicionartime(input.value)
     console.log(campeonato);
-    timenatabela()
+    atualizartabela()
     atualizaOpcoes()
     input.value = ""
 })
 
-
+let registrar = document.getElementById("registrar")
+let selectorC = document.getElementById("casa")
+let selectorV = document.getElementById("visitante")
+let inputC = document.getElementById("golsCasa")
+let inputV = document.getElementById("golsVisitante")
+registrar.addEventListener("click", () => {
+    let timeCasa = campeonato.times.find(T => T.nome === selectorC.value)
+    let timeVisitante = campeonato.times.find(T => T.nome === selectorV.value)
+    
+    let partida = new Partida(timeCasa, timeVisitante)
+    partida.jogo(Number(inputC.value), Number(inputV.value))
+    atualizartabela()
+    
+})
