c71b4ec10349cefc585397406a3264015a327569
niks-b1 <nikolasbreno.vs@gmail.com>
2026-02-28 12:28:46 -0300

    POO

--- POO/campeonato_futebol.js
+++ POO/campeonato_futebol.js
@@ -51,9 +51,6 @@ class Campeonato{
 }
 let campeonato = new Campeonato()
 
-let input = document.getElementById("nomedotime")
-let botaotime = document.getElementById("adicionar")
-
 let tabela = document.getElementById("tabela")
 let timeatual = campeonato.times[campeonato.times.length -1]
 function atualizartabela(){
@@ -61,14 +58,14 @@ function atualizartabela(){
     for(let contador = 0;contador < campeonato.times.length;contador ++){
         let novalinha = document.createElement("tr")
         novalinha.innerHTML = 
-            "<td>" + campeonato.times[contador].nome + "</td>" +
-            "<td>" + campeonato.times[contador].calcularpontos() + "</td>" +
-            "<td>" + campeonato.times[contador].vitoria + "</td>" +
-            "<td>" + campeonato.times[contador].empate + "</td>" +
-            "<td>" + campeonato.times[contador].derrota + "</td>" +
-            "<td>" + campeonato.times[contador].golsmarcados + "</td>" +
-            "<td>" + campeonato.times[contador].golssofridos + "</td>" +
-            "<td>" + campeonato.times[contador].calcularsaldo() +"</td>"
+        "<td>" + campeonato.times[contador].nome + "</td>" +
+        "<td>" + campeonato.times[contador].calcularpontos() + "</td>" +
+        "<td>" + campeonato.times[contador].vitoria + "</td>" +
+        "<td>" + campeonato.times[contador].empate + "</td>" +
+        "<td>" + campeonato.times[contador].derrota + "</td>" +
+        "<td>" + campeonato.times[contador].golsmarcados + "</td>" +
+        "<td>" + campeonato.times[contador].golssofridos + "</td>" +
+        "<td>" + campeonato.times[contador].calcularsaldo() +"</td>"
         tabela.appendChild(novalinha)
     }
 }
@@ -83,25 +80,45 @@ function atualizaOpcoes(){
     selecionartime[1].appendChild(novaopcao2)
 }
 
+let input = document.getElementById("nomedotime")
+let botaotime = document.getElementById("adicionar")
+let alertaTime = document.getElementById("alertatime")
 botaotime.addEventListener("click", () => {
+    if(input.value.length <= 2){
+        alertaTime.innerText = "o nome do time deve conter pelo menos 3 caracteres"
+        return
+    }
+    let alerta1 = campeonato.times.find(T => T.nome === input.value)
+    if(alerta1 != null){
+       alertaTime.innerText = "esse time já foi cadastrado"
+       return
+    }
     campeonato.adicionartime(input.value)
     console.log(campeonato);
     atualizartabela()
     atualizaOpcoes()
     input.value = ""
+    alertaTime.innerText = ""
 })
 
+let alertaPartida = document.getElementById("alertapartida")
 let registrar = document.getElementById("registrar")
 let selectorC = document.getElementById("casa")
 let selectorV = document.getElementById("visitante")
 let inputC = document.getElementById("golsCasa")
 let inputV = document.getElementById("golsVisitante")
 registrar.addEventListener("click", () => {
+    if(selectorC.value === selectorV.value){
+        alertaPartida.innerText = "um time não pode jogar contra si"
+        return
+    }
     let timeCasa = campeonato.times.find(T => T.nome === selectorC.value)
     let timeVisitante = campeonato.times.find(T => T.nome === selectorV.value)
     
     let partida = new Partida(timeCasa, timeVisitante)
     partida.jogo(Number(inputC.value), Number(inputV.value))
     atualizartabela()
-    
+    alertaPartida.innerText = ""
+    inputC.value = ""
+    inputV.value = ""
 })
