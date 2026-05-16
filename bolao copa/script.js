b32089bd21dd9a31e445d005a3c83af20730f16c
niks-b1 <nikolasbreno.vs@gmail.com>
2026-05-16 12:17:31 -0300

    bolao

--- bolao copa/script.js
+++ bolao copa/script.js
@@ -1,7 +1,42 @@
+function geraPartidas(g, htmlpai){
+
+for(let posicaopais = 0; posicaopais < g.paises.length-1; posicaopais++){
+
+    let nomepais = g.paises[0]
+    for(let paiscontra = posicaopais+1; paiscontra < g.paises.length; paiscontra++){
+
+      let htmlpartida = document.createElement("div")
+      htmlpartida.classList.add("")
+      htmlpartida.innerHTML = ``
+      htmlpai.appendChild(htmlpartida)
+    }
+  }
+}
+
+function geraGrupoHTML(g){
+    let htmlgrupo = document.createElement("div")
+    
+    htmlgrupo.classList.add("grupo")
+    htmlgrupo.innerHTML = `<h2>Grupo ${g.grupo}</h2> <p> ${g.paises}</p>`
+
+    return htmlgrupo
+}
+
+let listaGrupoHTML = document.querySelector("#grupos")
+
 async function carregarDados() {
-  const resposta = await fetch("json.json");
+  const resposta = await fetch("./json.json");
   const dados = await resposta.json();
-  console.log(dados);
+
+  let grupos = dados.grupos
+
+  for(let posicaoGrupo = 0; posicaoGrupo < grupos.length; posicaoGrupo++){
+    
+    let htmlgrupo = geraGrupoHTML(grupos[posicaoGrupo])
+    listaGrupoHTML.appendChild(htmlgrupo)
+    
+    geraPartidas(grupos[posicaoGrupo], htmlgrupo)
+  }
 }
 
 carregarDados();
